d9aa0b2c04e2714c9d00e8be78e0f17948dff1a8
UnbrokenRogue <tkoch115@gmail.com>
2023-11-17 20:29:54 -0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+
+export default function Page() {
+  return <h1>Hello, Next.js!</h1>
+}
+
+// "use client"
+// import CountButton from "./CountButton"
+// export default function Page() {
+//     return (
+//     <div>
+//     <CountButton 
+//     incrementval = {1}
+//     //name = {"eric"}
+//     />
+//     <CountButton 
+//     incrementval = {2}
+//     //name = {"john"}
+//     />
+//     {/* <CountButton /> */}
+
+//     <hr />
+//     <a href="https://github.com/UnbrokenRogue/reactjsfor331.git">Link to my github</a>
+//     <hr />
+//     <p>Search Bar</p>
+//     </div>
+//     )
+//   }
+
+// //   export default function Page() {
+// //     // return <h1>Hello, Next.js!</h1>
+// //     return <CountButton />
+// //   }
