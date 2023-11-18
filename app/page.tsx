ad837b01c1df7053df48dbe7528e6034c56ed97c
treyton.koch <m27p251@csci331.cs.montana.edu>
2023-11-17 21:18:21 -0700

    Changed port number

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,32 @@
 
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
-}
+//export default function Page() {
+  //return <h1>Howdy!</h1>
+//}
 
-// "use client"
-// import CountButton from "./CountButton"
-// export default function Page() {
-//     return (
-//     <div>
-//     <CountButton 
-//     incrementval = {1}
-//     //name = {"eric"}
-//     />
-//     <CountButton 
-//     incrementval = {2}
-//     //name = {"john"}
-//     />
-//     {/* <CountButton /> */}
+ "use client"
+ import CountButton from "./CountButton"
+ export default function Page() {
+     return (
+     <div>
+     <CountButton 
+     incrementval = {1}
+     //name = {"eric"}
+     />
+     <CountButton 
+     incrementval = {2}
+     //name = {"john"}
+     />
+     {/* <CountButton /> */}
 
-//     <hr />
-//     <a href="https://github.com/UnbrokenRogue/reactjsfor331.git">Link to my github</a>
-//     <hr />
-//     <p>Search Bar</p>
-//     </div>
-//     )
-//   }
+     <hr />
+     <a href="https://github.com/UnbrokenRogue/NextJSHW.git">Link to my github</a>
+     <hr />
+     <p>Search Bar</p>
+     </div>
+     )
+   }
 
-// //   export default function Page() {
-// //     // return <h1>Hello, Next.js!</h1>
-// //     return <CountButton />
-// //   }
+ //   export default function Page() {
+ //     // return <h1>Hello, Next.js!</h1>
+ //     return <CountButton />
+ //   }
